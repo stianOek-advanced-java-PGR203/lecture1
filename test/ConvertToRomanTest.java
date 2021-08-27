b6fd6ce65300ebfcfe26ee5fc38843fc225a21b8
StianOek <66445281+StianOek@users.noreply.github.com>
2021-08-27 09:32:20 +0200

    shouldConvert1ToI(fails)

--- test/ConvertToRomanTest.java
+++ test/ConvertToRomanTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ConvertToRomanTest {
+
+    @Test
+    void shouldConvert1ToI() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int i) {
+        return null;
+    }
+
 }
